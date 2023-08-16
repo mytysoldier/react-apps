0d107d4b5be1ae9b262ccc7d338d97d9744447cf
mytysoldier <yoshi008t@gmail.com>
2023-08-16 20:17:07 +0900

    fix

--- checklists/pages/index.tsx
+++ checklists/pages/index.tsx
@@ -89,13 +89,9 @@ export default function Home() {
                   .collection("todos")
                   .where("title", "==", title)
                   .get();
-                console.log(
-                  `querySnapshot: ${JSON.stringify(querySnapshot.docs)}`
-                );
                 querySnapshot.forEach(async (doc) => {
                   // DBから指定のドキュメントを削除
                   await doc.ref.delete();
-                  console.log("deleted");
                 });
                 // 画面に反映
                 setTitleList(
