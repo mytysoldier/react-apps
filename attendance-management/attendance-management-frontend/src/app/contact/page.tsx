a44605237c580ed1f6e9a89c980d1504b76462ba
mytysoldier <yoshi008t@gmail.com>
2023-09-02 18:37:49 +0900

    update

---
+++ attendance-management/attendance-management-frontend/src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+type Props = {};
+
+export default function Page() {
+  return (
+    <div className="flex h-full flex-col justify-center items-center">
+      <h1 className="text-4xl mb-5 font-bold">Contact</h1>
+      <span className="text-7xl">📞</span>
+    </div>
+  );
+}
