9e7119b0e6585ab32320595e76e50bda24b3eace
mytysoldier <yoshi008t@gmail.com>
2023-09-22 22:28:58 +0900

    update

--- attendance-management/attendance-management-backend/schema/attendance/funcs.py
+++ attendance-management/attendance-management-backend/schema/attendance/funcs.py
@@ -6,4 +6,14 @@ from datetime import date
 
 def register_attendance(status: str):
     created_attendance = create_attendance(engine, status)
-    return Attendance(id=created_attendance.id, date=created_attendance.date)
+    return Attendance(
+        id=created_attendance.id,
+        date=created_attendance.date,
+        start_time=created_attendance.start_time,
+        end_time=created_attendance.end_time,
+        status=created_attendance.status,
+        work_time=created_attendance.work_time,
+        over_work_time=created_attendance.over_work_time,
+        late_night_work_time=created_attendance.late_night_work_time,
+        holiday_work_time=created_attendance.holiday_work_time,
+    )
