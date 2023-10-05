d6513d1ecb671bf00f4c4c4dc4babb6f0bd997e0
mytysoldier <yoshi008t@gmail.com>
2023-10-06 08:23:03 +0900

    update

--- attendance-management/attendance-management-frontend/src/components/attendance/attendance_report/AttendanceReport.tsx
+++ attendance-management/attendance-management-frontend/src/components/attendance/attendance_report/AttendanceReport.tsx
@@ -37,10 +37,73 @@ const COLUMNS = [
   "休日労働時間",
 ];
 
+// その月の日にちを一覧取得
+const getDaysInMonth = (year: number, month: number): Date[] => {
+  const startDate = new Date(year, month - 1, 1);
+  const endDate = new Date(year, month, 0);
+  const daysInMonth = [];
+  for (
+    let date = startDate;
+    date <= endDate;
+    date.setDate(date.getDate() + 1)
+  ) {
+    daysInMonth.push(new Date(date));
+  }
+  return daysInMonth;
+};
+
+// 月のレポートデータを生成
+const generateMonthlyData = (
+  year: number,
+  month: number,
+  data: AttendanceData[]
+) => {
+  const monthDates = getDaysInMonth(year, month);
+  const monthlyData = monthDates.map((date) => {
+    const attendanceData = data.find((item) => {
+      const itemDate = new Date(item.date);
+      return itemDate.getDate() === date.getDate();
+    });
+
+    if (attendanceData) {
+      return {
+        date: date,
+        start_time: attendanceData.start_time,
+        end_time: attendanceData.end_time,
+        status: attendanceData.status,
+        work_time: attendanceData.work_time,
+        over_work_time: attendanceData.over_work_time,
+        late_night_work_time: attendanceData.late_night_work_time,
+        holiday_work_time: attendanceData.holiday_work_time,
+      };
+    } else {
+      return {
+        date: date,
+        start_time: null,
+        end_time: null,
+        status: "",
+        work_time: null,
+        over_work_time: null,
+        late_night_work_time: null,
+        holiday_work_time: null,
+      };
+    }
+  });
+
+  return monthlyData;
+};
+
 const convertAttendanceDataToString = (data: AttendanceData[]) => {
-  return data.map((item) => [
+  // 今月のデータを生成
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  const currentMonth = currentDate.getMonth() + 1;
+  const monthlyData = generateMonthlyData(currentYear, currentMonth, data);
+
+  // データを変換
+  const convertedData = monthlyData.map((item) => [
     format(item.date, "MM/dd(EEE)", { locale: ja }),
-    format(item.start_time, "HH:mm"),
+    item.start_time ? format(item.start_time, "HH:mm") : "",
     item.end_time != null ? format(item.end_time, "HH:mm") : "",
     item.status,
     item.work_time?.toString() ?? "0",
@@ -48,6 +111,19 @@ const convertAttendanceDataToString = (data: AttendanceData[]) => {
     item.late_night_work_time?.toString() ?? "0",
     item.holiday_work_time?.toString() ?? "0",
   ]);
+
+  return convertedData;
+
+  // return data.map((item) => [
+  //   format(item.date, "MM/dd(EEE)", { locale: ja }),
+  //   format(item.start_time, "HH:mm"),
+  //   item.end_time != null ? format(item.end_time, "HH:mm") : "",
+  //   item.status,
+  //   item.work_time?.toString() ?? "0",
+  //   item.over_work_time?.toString() ?? "0",
+  //   item.late_night_work_time?.toString() ?? "0",
+  //   item.holiday_work_time?.toString() ?? "0",
+  // ]);
 };
 
 export const AttendanceReport = () => {
